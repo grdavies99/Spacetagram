c6d085b9bbe4dccca766dd0d08be9e9ce4cb19ae
Garrett Davies <grdavies99@gmail.com>
2022-01-10 10:49:44 -0700

    + frontend Template, Like button animation, Ajax

--- functions.js
+++ functions.js
@@ -1,1 +1,21 @@
+function LikeAnimation(name){
+        $('i#title-of-the-image').toggleClass('fa-heart-animation');
+}
+
+function LoadContent(){
+    var pictureObject = "";
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            
+            pictureObject = JSON.parse(this.responseText);
+            console.log(pictureObject.explanation);
+        }
+    };
+        
+    xmlhttp.open("GET", "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", true);
+    xmlhttp.send();
+    return pictureObject.explanation;
+}
+
 
