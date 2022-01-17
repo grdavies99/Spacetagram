a94a6f958c54ed86cc384096d7a42e01aca52b74
Garrett Davies <grdavies99@gmail.com>
2022-01-17 13:20:00 -0700

    api calls are on an interval now and can render new elements

--- functions.js
+++ functions.js
@@ -7,19 +7,35 @@ function LoadContent(){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            
             pictureObject = JSON.parse(this.responseText);
-            $('p.card-text').html(pictureObject.explanation);
-            $('img.card-img').attr('src', pictureObject.url);
-            $('h5.card-title').html('<button class=" like-review" onclick="LikeAnimation(\'title of the image\');"><i class="fa fa-heart" id="title-of-the-image"></i> </button>' 
-            + pictureObject.title);
-            // $('i#title-of-the-image').attr('data-title', pictureObject.title);
+            console.log(pictureObject);
+            for(var i = 0; i < pictureObject.length; i++){
+                if(pictureObject[i].media_type =="image"){
+                prepareCard(pictureObject[i].date);
+            $('p.card-text.'+pictureObject[i].date).html(pictureObject[i].explanation);
+            $('img.card-img.'+pictureObject[i].date).attr('src', pictureObject[i].url);
+            $('h5.card-title.'+pictureObject[i].date).html('<button class=" like-review" onclick="LikeAnimation(\'title of the image\');"><i class="fa fa-heart" id="title-of-the-image"></i> </button>' 
+            + pictureObject[i].title  +" - " + pictureObject[i].date);
+            
+                }
+            }
+
         }
     };
         
-    xmlhttp.open("GET", "https://api.nasa.gov/planetary/apod?api_key=E0ivgdohXIG7nAB0msyOhb5LpZEDQX6YYDWOGrIR", true);
+    xmlhttp.open("GET", "https://api.nasa.gov/planetary/apod?api_key=E0ivgdohXIG7nAB0msyOhb5LpZEDQX6YYDWOGrIR&count=3", true);
     xmlhttp.send();
-    
 }
+
+function prepareCard(date){
+    $('div.container').append(
+        '<div class="row">\n<div class="col-md-6">\n<div class="card">\n<img class="card-img '+date+'" src="">\n<div class="card-body "><i class="bi bi-heart-fill"></i><h5 class="card-title '+date+'"><button class=" like-review" onclick="LikeAnimation("title of the image");"><i class="fa fa-heart" id="title-of-the-image"></i> </button> title of the image</h5>\n<p class="card-text '+date+'"></p>\n</div>\n</div>\n</div>\n</div>'
+    );
+}
+
+// function renderImages(){
+//     myinterval = LoadContent, 1000);
+    
+// }
 
 
